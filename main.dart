1e3f4b866ebddb273538b6a7b9b9480d382f197e
MD. JONAYED HOSSAIN OPI <134625691+opi1001@users.noreply.github.com>
2024-02-01 21:10:51 +0600

    Add files via upload
    🚀 Just I am doing Dart's Class, Inheritance, and Abstract topics! 🎓

---
+++ main.dart
@@ -0,0 +1,40 @@
+void main() {
+  // var h = HR();
+  // var s = Sales();
+  // h.salary();
+}
+
+class Employee {
+  var id;
+  var name;
+  var age;
+
+  Employee(this.id, this.age, this.name) {
+    id = id;
+    name = name;
+    age = age;
+    print('id');
+    print('id');
+    print('name');
+  }
+
+  // void salary() {
+  //   print("Employee's salary");
+  // }
+}
+
+
+// class HR extends Employee {
+//   @override
+//   void salary() {
+//     print("HR's Salary");
+//   }
+// }
+
+// class Sales extends HR {}
+
+// class Marketing extends Employee {
+//   void salesBonus() {
+//     print("Total amount is 3333");
+//   }
+// }
